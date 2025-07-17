bb7512f4d8947f20197f08d2645dca3bf9209edd
Aniket <144130684+Aniketsharma09@users.noreply.github.com>
2025-07-17 11:46:59 +0530

    Updated Backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,15 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
 
-const PORT = process.env.PORT || 3000;
+// Disable static file serving (no 'public' folder)
+const middlewares = jsonServer.defaults({ static: null });
+
+const port = process.env.PORT || 8080;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
 });
